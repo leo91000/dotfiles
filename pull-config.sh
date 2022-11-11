786e57db5bb219ae5484f183072ede367ccfcd52
Léo Coletta <leo.coletta@pm.me>
2022-11-11 16:14:37 +0100

    feat: Update config

---
+++ pull-config.sh
@@ -0,0 +1,4 @@
+git pull
+mkdir -p ~/.config/nvim/lua
+cp ./nvim/init.lua ~/.config/nvim/init.lua
+cp -r ./nvim/lua ~/.config/nvim/lua
--- update-config.sh
+++ update-config.sh
@@ -2,7 +2,6 @@
 
 mkdir -p nvim
 cp ~/.config/nvim/init.lua ./nvim
-cp -r ~/.config/nvim/lua ./nvim
 git add .
 git commit -m "feat: Update config"
 git push
