8b2cc99becd0bf9461415ade3d42b5298418a63c
Léo Coletta <contact@leo-coletta.fr>
2022-11-14 22:23:58 +0100

    feat: Update config

--- nvim/init.lua
+++ nvim/init.lua
@@ -206,7 +206,20 @@ require'nvim-treesitter.configs'.setup {
 }
 
 -- scrollbar setup
-require("scrollbar").setup()
+local colors = require("tokyonight.colors").setup()
+require("scrollbar").setup({
+    handle = {
+        color = colors.fg_gutter,
+    },
+    marks = {
+        Search = { color = colors.orange },
+        Error = { color = colors.error },
+        Warn = { color = colors.warning },
+        Info = { color = colors.info },
+        Hint = { color = colors.hint },
+        Misc = { color = colors.purple },
+    }
+})
 
 -- lualine setup
 require('lualine').setup {
