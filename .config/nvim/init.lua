27395ddcf1a50600ea8ffa0078bc8b4e28bb2ba7
Léo Coletta <contact@leo-coletta.fr>
2022-11-16 09:50:56 +0100

    feat: Update config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -292,3 +292,14 @@ vim.api.nvim_create_autocmd("BufWritePre", {
 })
 
 lspconfig.eslint.setup{}
+
+-- Copy to clipboard
+vim.keymap.set('v', '<leader>y', '"+y')
+vim.keymap.set('n', '<leader>Y', '"+yg_')
+vim.keymap.set('n', '<leader>y', '"+y')
+
+-- Paste from clipboard
+vim.keymap.set('n', '<leader>p', '"+p')
+vim.keymap.set('n', '<leader>P', '"+P')
+vim.keymap.set('v', '<leader>p', '"+p')
+vim.keymap.set('v', '<leader>P', '"+P')
