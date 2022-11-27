766bc7419fa98f8f4a95ff2300071948697fa8cd
Léo Coletta <leo.coletta@pm.me>
2022-11-27 15:51:22 +0100

    feat: Update config

--- pull-config.sh
+++ pull-config.sh
@@ -4,3 +4,5 @@ mkdir -p ~/.config/alacritty
 mkdir -p ~/.config/nvim
 cp ./.config/nvim/init.lua ~/.config/nvim/init.lua
 cp ./.config/alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml
+cp ./.tmux.conf ~/.tmux.conf
+cp ./.zshrc ~/.zshrc
--- update-config.sh
+++ update-config.sh
@@ -3,6 +3,8 @@
 mkdir -p nvim
 cp ~/.config/nvim/init.lua ./.config/nvim/init.lua
 cp ~/.config/alacritty/alacritty.yml ./.config/alacritty/alacritty.yml
+cp ~/.tmux.conf ./.tmux.config
+cp ~/.zshrc ./.zshrc
 git add .
 git commit -m "feat: Update config"
 git push
