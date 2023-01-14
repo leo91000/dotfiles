4f89f63c56486c248374b5a997d6e1ceb3ddad0f
Léo Coletta <contact@leo-coletta.fr>
2023-01-14 22:38:53 +0100

    feat: Update config

--- pull-config.sh
+++ pull-config.sh
@@ -3,7 +3,7 @@ git pull
 mkdir -p ~/.config/alacritty
 mkdir -p ~/.config/nvim/core
 cp ./.config/nvim/init.lua ~/.config/nvim/init.lua
-cp -r ./.config/nvim/core ~/.config/nvim/core
+cp -r ./.config/nvim/core ~/.config/nvim
 cp ./.config/alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml
 cp ./.tmux.conf ~/.tmux.conf
 cp ./.zshrc ~/.zshrc
