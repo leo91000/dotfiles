09c7e0c6fcf42778dcf841631c75e5a56ec0ab30
Léo Coletta <contact@leo-coletta.fr>
2023-01-14 22:38:31 +0100

    feat: Update config

--- update-config.sh
+++ update-config.sh
@@ -2,7 +2,7 @@
 
 mkdir -p .config/nvim/core
 cp ~/.config/nvim/init.lua ./.config/nvim/init.lua
-cp -r ~/.config/nvim/core ./.config/nvim/core
+cp -r ~/.config/nvim/core ./.config/nvim
 cp ~/.config/alacritty/alacritty.yml ./.config/alacritty/alacritty.yml
 cp ~/.tmux.conf ./.tmux.conf
 cp ~/.zshrc ./.zshrc
