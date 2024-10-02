a26a0e70bd5424e84e60552b3b258a2e1b11a88e
Léo Coletta <mail.leo.coletta@gmail.com>
2024-10-02 10:57:58 +0200

    Update .config/nvim/lua/plugins/astrolsp.lua

--- dot_config/nvim/lua/plugins/astrolsp.lua
+++ dot_config/nvim/lua/plugins/astrolsp.lua
@@ -82,9 +82,10 @@ return {
 				"ts_ls",
 			},
 			timeout_ms = 1000, -- default format timeout
-			-- filter = function(client) -- fully override the default formatting function
-			--   return true
-			-- end
+			filter = function(client)
+				vim.notify("Formatting with " .. client.name)
+				return true
+			end,
 		},
 		-- enable servers that you already have installed without mason
 		servers = {
