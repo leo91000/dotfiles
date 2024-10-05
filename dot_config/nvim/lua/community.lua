112122a78a39704743607424196b8dff900e59f9
Léo Coletta <mail.leo.coletta@gmail.com>
2024-10-06 00:20:38 +0200

    Update .config/nvim/lua/community.lua

--- dot_config/nvim/lua/community.lua
+++ dot_config/nvim/lua/community.lua
@@ -4,6 +4,6 @@ return {
 	{ import = "astrocommunity.pack.lua" },
 	{ import = "astrocommunity.pack.rust" },
 	-- { import = "astrocommunity.completion.copilot-lua" },
-	{ import = "astrocommunity.completion.supermaven" },
+	{ import = "astrocommunity.completion.supermaven-nvim" },
 	{ import = "astrocommunity.utility.telescope-live-grep-args-nvim" },
 }
