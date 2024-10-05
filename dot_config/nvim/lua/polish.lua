1660dcefdffe8fa807807586938639d492563a8a
Léo Coletta <mail.leo.coletta@gmail.com>
2024-10-06 00:30:01 +0200

    Update .config/nvim/lua/polish.lua

--- dot_config/nvim/lua/polish.lua
+++ dot_config/nvim/lua/polish.lua
@@ -1,4 +1,4 @@
-vim.filetype.add { extension = { wgsl = "wgsl" } }
+vim.filetype.add({ extension = { wgsl = "wgsl" } })
 vim.wo.foldmethod = "expr"
 vim.wo.foldexpr = "nvim_treesitter#foldexpr()"
 vim.o.foldlevelstart = 99
