5855de0c16479d56f730bffd41a84158773a78c2
Léo Coletta <mail.leo.coletta@gmail.com>
2024-10-23 11:50:14 +0200

    Update .config/nvim/lua/plugins/mason.lua

--- dot_config/nvim/lua/plugins/mason.lua
+++ dot_config/nvim/lua/plugins/mason.lua
@@ -6,7 +6,7 @@ return {
 		opts = function(_, opts)
 			opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
 				"eslint",
-				"volar",
+				"vue-language-server",
 				"rust_analyzer",
 				"cssls",
 				"dockerls",
