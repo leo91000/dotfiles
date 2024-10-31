5ee796775679066b905154d449c9a6252ab0c340
Léo Coletta <mail.leo.coletta@gmail.com>
2024-10-31 10:19:10 +0100

    Update .config/nvim/lua/plugins/astrocore.lua

--- dot_config/nvim/lua/plugins/astrocore.lua
+++ dot_config/nvim/lua/plugins/astrocore.lua
@@ -105,11 +105,7 @@ return {
 
 						for _, buf in ipairs(vim.api.nvim_list_bufs()) do
 							if vim.api.nvim_buf_is_loaded(buf) then
-								vim.api.nvim_set_option_value(
-									"relativenumber",
-									not is_relativenumber,
-									{ scope = "local", buf = buf }
-								)
+								vim.api.nvim_set_option_value("relativenumber", not is_relativenumber, { buf = buf })
 							end
 						end
 					end,
