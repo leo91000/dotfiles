5dbac84afe237e02ec850059605d3447a3de2240
Léo Coletta <mail.leo.coletta@gmail.com>
2024-11-22 18:42:30 +0100

    Update .config/nvim/lua/polish.lua

--- dot_config/nvim/lua/polish.lua
+++ dot_config/nvim/lua/polish.lua
@@ -1,3 +1,6 @@
 vim.wo.foldmethod = "expr"
 vim.wo.foldexpr = "nvim_treesitter#foldexpr()"
 vim.o.foldlevelstart = 99
+
+vim.keymap.set("i", "<C-BS>", "<C-w>", { noremap = true })
+vim.keymap.set("i", "<C-Del>", "<C-o>dw", { noremap = true })
