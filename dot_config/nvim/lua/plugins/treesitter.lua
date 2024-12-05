60fd3d0983ed57c9899bd0939dcb48e0bf3ea7c8
Léo Coletta <mail.leo.coletta@gmail.com>
2024-12-05 14:43:16 +0100

    Update .config/nvim/lua/plugins/treesitter.lua Update .config/nvim/lua/polish.lua

--- dot_config/nvim/lua/plugins/treesitter.lua
+++ dot_config/nvim/lua/plugins/treesitter.lua
@@ -1,25 +1,26 @@
 ---@type LazySpec
 return {
-  "nvim-treesitter/nvim-treesitter",
-  opts = function(_, opts)
-    opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
-      "lua",
-      "python",
-      "javascript",
-      "typescript",
-      "html",
-      "css",
-      "json",
-      "yaml",
-      "toml",
-      "bash",
-      "tsx",
-      "vue",
-      "svelte",
-      "prisma",
-      "svelte",
-      "zig",
-      "wgsl",
-    })
-  end,
+	"nvim-treesitter/nvim-treesitter",
+	opts = function(_, opts)
+		opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
+			"lua",
+			"python",
+			"javascript",
+			"typescript",
+			"html",
+			"css",
+			"json",
+			"yaml",
+			"toml",
+			"bash",
+			"tsx",
+			"vue",
+			"svelte",
+			"prisma",
+			"svelte",
+			"zig",
+			"wgsl",
+			"embedded_template",
+		})
+	end,
 }
