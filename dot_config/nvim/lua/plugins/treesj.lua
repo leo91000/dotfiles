aecd30e4c47ad933e955cb23603d701b0427ddaa
Léo Coletta <mail.leo.coletta@gmail.com>
2025-01-08 16:57:56 +0100

    Update .config/nvim/lua/plugins/treesj.lua

--- dot_config/nvim/lua/plugins/treesj.lua
+++ dot_config/nvim/lua/plugins/treesj.lua
@@ -4,7 +4,7 @@ return {
 	dependencies = { "nvim-treesitter/nvim-treesitter" },
 	config = function()
 		require("treesj").setup({
-			max_join_length = 500,
+			max_join_length = 100000,
 			use_default_keymaps = false,
 		})
 	end,
